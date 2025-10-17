1090c6a3bf72bd055897c37cb90adf26f11d08bd
Sushil Kamble <dev.rocketdevelopers@gmail.com>
2025-10-18 03:51:20 +0530

    feat: Increase revalidation time for blog posts to improve caching efficiency

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -57,18 +57,22 @@ export async function generateMetadata({
 async function getPaginatedPosts(page: number): Promise<BlogPost[]> {
   const start = (page - 1) * POSTS_PER_PAGE;
   const end = start + POSTS_PER_PAGE;
-  return client.fetch(postsQuery, { start, end }, { next: { revalidate: 60 } });
+  return client.fetch(
+    postsQuery,
+    { start, end },
+    { next: { revalidate: 3600 } }
+  );
 }
 
 async function getTotalPostsCount(): Promise<number> {
-  return client.fetch(postsCountQuery, {}, { next: { revalidate: 60 } });
+  return client.fetch(postsCountQuery, {}, { next: { revalidate: 3600 } });
 }
 
 async function getFeaturedPosts(): Promise<BlogPost[]> {
-  return client.fetch(featuredPostsQuery, {}, { next: { revalidate: 60 } });
+  return client.fetch(featuredPostsQuery, {}, { next: { revalidate: 3600 } });
 }
 
-export const revalidate = 60; // 60 minutes
+export const revalidate = 3600; // 3600 minutes
 export const dynamicParams = true;
 
 export default async function BlogPage({ searchParams }: Props) {
